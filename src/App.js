01196e60a0beadae35bd56a08fd49f1ae6763e29
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-04-30 12:42:38 +0300

    programmatic navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
 
 function App() {
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const navigate= useNavigate()
+  const login = () => {
+    setIsLoggedIn(true)
+  }
+  const logout= () =>{
+    setIsLoggedIn(false)
+  }
+  useEffect(()=>{
+    if (isLoggedIn) {
+      navigate('/')
+    } else {
+      navigate('/login')
+    }
+  },[isLoggedIn, navigate])
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+     
+      {isLoggedIn?  <NavBar logout={logout} /> : <Navigate to='/login' /> }
+      <Outlet context={login}/>
     </div>
   );
 }
